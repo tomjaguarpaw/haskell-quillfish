9d4ca8adc3a56b294cf1519929c6a0a830ea00c8
Tom Ellis <tom-2013@purelyagile.com>
2015-02-28 15:55:01 +0000

    Use Dynamic

--- Main.hs
+++ Main.hs
@@ -2,7 +2,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs #-}
 
-import Data.Typeable
+import Data.Dynamic (Dynamic, fromDynamic, toDyn)
+import qualified Data.Dynamic as Dynamic
+import Data.Typeable (Typeable)
 import qualified Data.Map as Map
 import Data.Map (Map)
 import Data.Monoid
@@ -12,32 +14,30 @@ import Debug.Trace
 import qualified Data.List.NonEmpty as NEL
 import qualified Control.Monad.Trans.State as St
 
-data Raw = forall a. Typeable a => Raw a
-
 type Failure = String
 
 type ColId = Int
 
 data Rows a = Rows { elements   :: [a]
-                   , col        :: ColId -> Q (a -> Raw)
-                   , indices    :: [(ColId, Raw)] -> Q [a] }
+                   , col        :: ColId -> Q (a -> Dynamic)
+                   , indices    :: [(ColId, Dynamic)] -> Q [a] }
 
 type Q = Either Failure
 
-castErr :: (Typeable a, Typeable b) => String -> a -> Q b
-castErr err a = case cast a of
+castErr :: Typeable b => String -> Dynamic -> Q b
+castErr err a = case Dynamic.fromDynamic a of
   Nothing -> Left ("Cast error: " <> err)
   Just b  -> return b
 
 rowsOfMap :: (Ord k, Typeable k, Typeable v, Eq v) => Map k v -> Rows (k, v)
 rowsOfMap m = Rows { elements = Map.toList m
-                   , col    = \i -> if i == 0 then return (Raw . fst)
-                                    else if i == 1 then return (Raw . snd)
+                   , col    = \i -> if i == 0 then return (toDyn . fst)
+                                    else if i == 1 then return (toDyn . snd)
                                          else Left ("Got " <> show i
                                                     <> " as index into a map")
                    , indices = \irs -> case firstMatch ((== 0) . fst) irs of
                        Nothing -> scanMap irs (Map.toList m)
-                       Just ((_, Raw r), as) -> do
+                       Just ((_, r), as) -> do
                          k <- castErr "index key" r
                          (scanMap as
                           . fmap (\v -> (k, v))
@@ -52,15 +52,15 @@ firstMatch p (a:as) = if p a
                       else fmap (\(b, bs) -> (b, a:bs)) (firstMatch p as)
 
 scanMap :: (Eq k, Eq v, Typeable k, Typeable v) =>
-           [(ColId, Raw)] -> [(k, v)] -> Q [(k, v)]
+           [(ColId, Dynamic)] -> [(k, v)] -> Q [(k, v)]
 scanMap irs l = fmap ($ l) (fmap concatEndo (mapM scan irs))
 
 concatEndo :: [a -> a] -> a -> a
 concatEndo = foldr (.) id
 
 scan :: (Eq k, Eq v, Typeable k, Typeable v) =>
-        (ColId, Raw) -> Q ([(k, v)] -> [(k, v)])
-scan (i, Raw r) = if i == 0
+        (ColId, Dynamic) -> Q ([(k, v)] -> [(k, v)])
+scan (i, r) = if i == 0
               then do
                     r' <- castErr "scan key 1" r
                     return (filter ((== r') . fst))
@@ -71,13 +71,13 @@ scan (i, Raw r) = if i == 0
               else Left "Got a wrong key index"
 
 
-join :: [a] -> Rows b -> [(a -> Raw, ColId)] -> Q [(a, b)]
+join :: [a] -> Rows b -> [(a -> Dynamic, ColId)] -> Q [(a, b)]
 join as rs fs = (fmap concat
                  . sequence
                  . map (\a -> (fmap.fmap) (\b -> (a, b)) (indices rs (map (\(f, i) -> (i, f a)) fs))))
                 as
 
-test = join as (rowsOfMap (Map.fromList (zip as as))) [(Raw, 0)]
+test = join as (rowsOfMap (Map.fromList (zip as as))) [(toDyn, 0)]
   where as = [1..1000000] :: [Int]
  
 data BaseTableColumn a = BaseTableColumn Tag
