73a762ddb34abbcf4d0464e8372d85ed41ba0e5e
Tom Ellis <tom-2013@purelyagile.com>
2015-02-28 15:45:22 +0000

    An AST

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GADTs #-}
 
 import Data.Typeable
 import qualified Data.Map as Map
@@ -8,14 +9,18 @@ import Data.Monoid
 import Data.Proxy
 import qualified Data.Foldable
 import Debug.Trace
+import qualified Data.List.NonEmpty as NEL
+import qualified Control.Monad.Trans.State as St
 
 data Raw = forall a. Typeable a => Raw a
 
 type Failure = String
 
+type ColId = Int
+
 data Rows a = Rows { elements   :: [a]
-                   , col        :: Int -> Q (a -> Raw)
-                   , indices    :: [(Int, Raw)] -> Q [a] }
+                   , col        :: ColId -> Q (a -> Raw)
+                   , indices    :: [(ColId, Raw)] -> Q [a] }
 
 type Q = Either Failure
 
@@ -47,14 +52,14 @@ firstMatch p (a:as) = if p a
                       else fmap (\(b, bs) -> (b, a:bs)) (firstMatch p as)
 
 scanMap :: (Eq k, Eq v, Typeable k, Typeable v) =>
-           [(Int, Raw)] -> [(k, v)] -> Q [(k, v)]
+           [(ColId, Raw)] -> [(k, v)] -> Q [(k, v)]
 scanMap irs l = fmap ($ l) (fmap concatEndo (mapM scan irs))
 
 concatEndo :: [a -> a] -> a -> a
 concatEndo = foldr (.) id
 
 scan :: (Eq k, Eq v, Typeable k, Typeable v) =>
-        (Int, Raw) -> Q ([(k, v)] -> [(k, v)])
+        (ColId, Raw) -> Q ([(k, v)] -> [(k, v)])
 scan (i, Raw r) = if i == 0
               then do
                     r' <- castErr "scan key 1" r
@@ -66,7 +71,7 @@ scan (i, Raw r) = if i == 0
               else Left "Got a wrong key index"
 
 
-join :: [a] -> Rows b -> [(a -> Raw, Int)] -> Q [(a, b)]
+join :: [a] -> Rows b -> [(a -> Raw, ColId)] -> Q [(a, b)]
 join as rs fs = (fmap concat
                  . sequence
                  . map (\a -> (fmap.fmap) (\b -> (a, b)) (indices rs (map (\(f, i) -> (i, f a)) fs))))
@@ -74,3 +79,25 @@ join as rs fs = (fmap concat
 
 test = join as (rowsOfMap (Map.fromList (zip as as))) [(Raw, 0)]
   where as = [1..1000000] :: [Int]
+ 
+data BaseTableColumn a = BaseTableColumn Tag
+
+data Column a where
+  BaseTableAttrExpr :: BaseTableColumn a -> Column a
+  Eq1 :: BaseTableColumn a -> Column a -> Column Bool
+  Eq2 :: Column a -> BaseTableColumn a -> Column Bool
+  Eq12 :: BaseTableColumn a -> BaseTableColumn a -> Column Bool
+  Fmap :: (a -> b) -> Column a -> Column b
+  Pure :: a -> Column a
+  Ap :: Column (a -> b) -> Column a -> Column b
+
+data PrimQuery = Unit
+               | forall a. BaseTable (Rows a)
+               | Product [NEL.NonEmpty PrimQuery]
+               | Restrict [Column Bool]
+
+type BaseTableId = Int
+
+type Tag = (BaseTableId, ColId)
+
+type Query a = St.State (Tag, PrimQuery) a
